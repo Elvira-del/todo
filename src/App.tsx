56e7a1cdeec38978e5d23e709eb1f802a497f816
Elvira-del <ninelostr@gmail.com>
2023-02-19 02:29:15 +0300

    Revert "feat: add base theme for app"
    This reverts commit 8b7d4f9bb83e3ea6356e08b43d2d448db1413110.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import { baseTheme } from './styles/theme';
-import GlobalStyle from './styles/global';
-
-import { TaskApp } from './components/TaskApp/TaskApp';
 
 function App() {
   return (
-    <ThemeProvider theme={baseTheme}>
-      <GlobalStyle />
-      <TaskApp />
-    </ThemeProvider>
+    <div className="App">
+      
+    </div>
   );
 }
 
