2160c09c74c69688c6ceb29a69cacfe5b26cbc21
Elvira-del <ninelostr@gmail.com>
2023-02-19 02:52:27 +0300

    feat: add button and checkbox ui elements

---
+++ src/UI/button/Button.tsx
@@ -0,0 +1,15 @@
+import React, { ComponentPropsWithoutRef, ReactNode } from "react";
+import { ButtonUI } from "../../styles/components";
+
+interface ButtonProps extends ComponentPropsWithoutRef<"button"> {
+  buttonColor: string;
+  children: ReactNode;
+}
+
+export const Button = ({ buttonColor, children, ...other }: ButtonProps) => {
+  return (
+    <ButtonUI bg={buttonColor} {...other}>
+      {children}
+    </ButtonUI>
+  );
+};
