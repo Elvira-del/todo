79bf720860880a62abaddc3d9f957ce592feee11
Elvira-del <ninelostr@gmail.com>
2023-02-22 03:51:59 +0300

    fix: remove unnecessary import

--- src/components/TaskApp/Footer/Footer.tsx
+++ src/components/TaskApp/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Container } from "styles/components";
 import { Task } from "../TaskApp";
 import * as Styled from "./StyledFooter";
